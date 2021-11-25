aca88f10d1872dc70ce871b1d599fdd151531879
Nahom Assefa <nzenebe@gmail.com>
2021-11-25 12:34:19 -0600

    API expired

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ var IdPlay;
 // Get Artist 
 var getData = function (artist, song, index) {
   // Format the YouTube API url
-  var apiUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${artist}${song}&maxResults=1&type=video&key=AIzaSyAwl6OYOGUNSDQLOOk2O7KKDPHJuEI2M-I`;
+  var apiUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${artist}${song}&maxResults=1&type=video&key=AIzaSyA2gSQ1nqtkt0AqrTla0h3si_c5SmquD6Q'`;
 
   var encApoUrl = encodeURI(apiUrl);
   
