da1534e971fad60ab107cf16275c5ebe00cc2bb5
Fernando Ortega <feortegas@gmail.com>
2021-11-28 13:46:06 -0600

    add tutorial modal load/close functions

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+const tutorialModalEl = document.querySelector("#tutorial-modal");
+const tutorialModalBgEl = document.querySelector("#tutorial-modal-bg");
+
+
+// load the tutorial Modal
+var loadTutorialModal = function() {
+    tutorialModalEl.classList.add("is-active");
+};
+
+// closes the modal when user clicks on the background
+tutorialModalBgEl.addEventListener("click", function() {
+    tutorialModalEl.classList.remove("is-active");
+});
+
+
+// load tutorial modal on page load
+loadTutorialModal();
