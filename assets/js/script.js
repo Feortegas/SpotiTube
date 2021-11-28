b8a8b5f6c48f37d08cd88d0bd902f78744e14ec5
Nahom Assefa <nzenebe@gmail.com>
2021-11-28 15:50:24 -0600

    new test

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,114 +6,117 @@
 //Fernando Youtube Cleint Id = '272643493783-for2qk69datv1od5bevqtvb0q2g3tifr.apps.googleusercontent.com';
 
 //Declarations
-var IdPlay; 
+var IdPlay;
 var playlistArr = [];
 var artistArr = [];
 var videoArr = [];
 
-// Get Artist 
+// Get Artist
 var getData = function () {
   var artist = localStorage.getItem("artist");
-  for(let index=0; index < playlistArr.length; index++) {
-  // Format the YouTube API url
-  var apiUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${artist}${playlistArr[index]}&maxResults=1&type=video&key=AIzaSyA2gSQ1nqtkt0AqrTla0h3si_c5SmquD6Q`;
+  for (let index = 0; index < playlistArr.length; index++) {
+    // Format the YouTube API url
+    var apiUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${artist}${playlistArr[index]}&maxResults=1&type=video&key=AIzaSyA2gSQ1nqtkt0AqrTla0h3si_c5SmquD6Q`;
 
-  var encApoUrl = encodeURI(apiUrl);
-  // Make a request to the url
-  fetch(encApoUrl)
-    .then(function (response) {
-      //Request was successful
-      if (response.ok) {
-        response.json().then(function (data) {
-          //console.log('line 24', data);
+    var encApoUrl = encodeURI(apiUrl);
+    // Make a request to the url
+    fetch(encApoUrl)
+      .then(function (response) {
+        //Request was successful
+        if (response.ok) {
+          response.json().then(function (data) {
+            //console.log('line 24', data);
 
-          if (data.items[0] && data.items[0].snippet.title.includes("(Official Music Video)") && index < 3) {
+            if (
+              data.items[0] &&
+              data.items[0].snippet.title.includes("(Official Music Video)") &&
+              index < 3
+            ) {
+              // push to video array
+              videoArr.push(data.items[0].id.videoId);
+              saveVideoId();
 
-            // push to video array
-            videoArr.push(data.items[0].id.videoId);
-            saveVideoId();
+              // insert thumbnails data into bulma css cards
+              document.querySelector(`#card-${index}`).className = "card";
+              document
+                .querySelector(`#video-${index}`)
+                .setAttribute(
+                  "src",
+                  data.items[0].snippet.thumbnails.default.url
+                );
+              document.querySelector(`#title-${index}`).textContent =
+                data.items[0].snippet.title;
 
-            // insert thumbnails data into bulma css cards
-            document.querySelector(`#card-${index}`).className = "card";
-            document.querySelector(`#video-${index}`).setAttribute("src", data.items[0].snippet.thumbnails.default.url);
-            document.querySelector(`#title-${index}`).textContent = data.items[0].snippet.title;
-
-
-            // $(`#video-${index}`).append(
-            //   `<h6>${data.items[0].snippet.title}</h6>`
-            // );
-            // $(`#video-${index}`).append(
-            //   `<a href=` +
-            //     "https://www.youtube.com/watch?v=" +
-            //     `${data.items[0].id.videoId} target="_blank"> <img src=${data.items[0].snippet.thumbnails.default.url}></a>`
-            // );
-            // Playlist Insert Function call
-            // playInsert(IdPlay, data.items[0].id.videoId);
-          }
-        
-        });
-      } else {
-        alert("Error: YouTube User Not Found");
-      }
-    })
-    .catch(function (error) {
-      alert("Unable to connect to YouTube");
-    });
-}
-JSON.parse(localStorage.getItem("videoId")).forEach(function(video){
-console.log(video);
-playInsert(IdPlay, video);
-});
+              // $(`#video-${index}`).append(
+              //   `<h6>${data.items[0].snippet.title}</h6>`
+              // );
+              // $(`#video-${index}`).append(
+              //   `<a href=` +
+              //     "https://www.youtube.com/watch?v=" +
+              //     `${data.items[0].id.videoId} target="_blank"> <img src=${data.items[0].snippet.thumbnails.default.url}></a>`
+              // );
+              // Playlist Insert Function call
+              // playInsert(IdPlay, data.items[0].id.videoId);
+            }
+          });
+        } else {
+          alert("Error: YouTube User Not Found");
+        }
+      })
+      .catch(function (error) {
+        alert("Unable to connect to YouTube");
+      });
+  }
+  console.log("Line70");
+  JSON.parse(localStorage.getItem("videoId")).forEach(function (video) {
+    console.log(video);
+    playInsert(IdPlay, video);
+  });
 };
 
-var saveVideoId = function() {
-    localStorage.setItem("videoId", videoArr);
-}
-
+var saveVideoId = function () {
+  localStorage.setItem("videoId", videoArr);
+};
 
 // Called After Sign In; retrieves playlist Id
-var playList = function() {
-    return gapi.client.youtube.playlists.list({
-      "part": [
-      "snippet,contentDetails"
-      ],
-      "maxResults": 25,
-      "mine": true
+var playList = function () {
+  return gapi.client.youtube.playlists
+    .list({
+      part: ["snippet,contentDetails"],
+      maxResults: 25,
+      mine: true,
     })
     .then(function (response) {
-        console.log('Returns playlist data: ', response)
-        IdPlay = response.result.items[0].id;
-      })
-      .catch(function (error) {
-        console.log('catch error retrieve playlist function');
-      });
-  };
-
+      console.log("Returns playlist data: ", response);
+      IdPlay = response.result.items[0].id;
+    })
+    .catch(function (error) {
+      console.log("catch error retrieve playlist function");
+    });
+};
 
 // Inserts Spotify Track Music Video To Playlist
-  var playInsert = function(playListId, videoId) {
-      console.log('99', playListId);
-      console.log('100', videoId);
-    return gapi.client.youtube.playlistItems.insert({
-      "part": [
-        "snippet"
-      ],
-      "resource": {
-        "snippet": {
-          "playlistId": playListId,
-          "position": 0,
-          "resourceId": {
-            "kind": "youtube#video",
-            "videoId": videoId
-          }
-        }
-      }
+var playInsert = function (playListId, videoId) {
+  console.log("99", playListId);
+  console.log("100", videoId);
+  return gapi.client.youtube.playlistItems
+    .insert({
+      part: ["snippet"],
+      resource: {
+        snippet: {
+          playlistId: playListId,
+          position: 0,
+          resourceId: {
+            kind: "youtube#video",
+            videoId: videoId,
+          },
+        },
+      },
     })
     .then(function (response) {
-        console.log('Playlist creation', response)
-      })
-      .catch(function (error) {
-        console.log('Catch error playlist insert function');
-      });
-  };
-    
+      console.log("Playlist creation", response);
+    })
+    .catch(function (error) {
+      console.log("Catch error playlist insert function");
+    });
+};
