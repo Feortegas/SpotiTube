065f0bcf583354f46a2e05d8b13d0d858ec569b1
Nahom Assefa <nzenebe@gmail.com>
2021-11-29 13:50:55 -0600

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,12 +42,10 @@ var getData = function () {
               console.log(data);
 
             // insert thumbnails data into bulma css cards
-            //document.querySelector(`#card-${index}`).classList.remove("is-hidden");
-            $(`#card-${index}`).removeClass("is-hidden");
+            document.querySelector(`#card-${index}`).classList.remove("is-hidden");
             $(`#video-${index}`).attr("src", data.items[0].snippet.thumbnails.default.url);
             $(`#title-${index}`).html(data.items[0].snippet.title);
-            //document.querySelector("#insert-youtube").classList.remove("is-hidden");
-            $("#insert-youtube").removeClass("is-hidden");
+            document.querySelector("#insert-youtube").classList.remove("is-hidden");
             }
           })
         } else {
